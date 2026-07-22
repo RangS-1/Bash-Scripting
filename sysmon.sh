39cc88e8eac002faf07ce01d4e258a4596602a62
RangS-1 <rangga19sj@gmail.com>
2026-07-22 18:59:54 +0700

    Update: tidying up output with :

--- sysmon.sh
+++ sysmon.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 echo ""
 get_ram(){
-    printf "RAM Used: " && free -h | grep Mem | awk '{print $3}'
+    printf "RAM Used  : " && free -h | grep Mem | awk '{print $3}'
 }
 
 get_disk(){
-    printf "Disk Used: " && df -h / | awk 'NR==2 {print $5}'
+    printf "Disk Used : " && df -h / | awk 'NR==2 {print $5}'
 }
 
 get_battery(){
-    printf "Battery: $(cat /sys/class/power_supply/BAT1/capacity)%%"
+    printf "Battery   : $(cat /sys/class/power_supply/BAT1/capacity)%%"
 }
 
 get_network(){
     NET_STATUS=$(nmcli networking connectivity)
     if [[ "$NET_STATUS" == "full" ]]; then
         echo ""
-        printf "Network: Connected"
+        printf "Network   : Connected"
     else
         echo ""
-        printf "Network: Not Connected"
+        printf "Network   : Not Connected"
     fi
 }
 
 get_ip(){
     echo ""
-    printf "Your IP: "
+    printf "Your IP   : "
     printf "$(ip address)" | grep "scope global" | awk '{print $2}'
 }
 
 get_uptime(){
-    printf "Uptime: " && uptime -p | awk '{print $2, $3, $4, $5}'
+    printf "Uptime    : " && uptime -p | awk '{print $2, $3, $4, $5}'
 }
 
 show_dashboard(){
@@ -52,7 +52,7 @@ update(){
         echo ""
 
         show_dashboard
-        
+
         sleep 2
     done
 }
