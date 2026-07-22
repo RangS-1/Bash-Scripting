b9d7d21225b9b467bf59586bc5148af0abaf9b22
RangS-1 <rangga19sj@gmail.com>
2026-07-22 18:32:36 +0700

    Update: sysmon.sh. Remove Comment

--- sysmon.sh
+++ sysmon.sh
@@ -14,14 +14,10 @@ get_disk(){
     printf "Disk Used: " && df -h / | awk 'NR==2 {print $5}'
 }
 
-#get_cpu(){
-#    printf "CPU Used: " && top | awk '{print $10}'
-#}
-#
 get_battery(){
     printf "Battery: $(cat /sys/class/power_supply/BAT1/capacity)%%"
 }
-#
+
 get_network(){
     NET_STATUS=$(nmcli networking connectivity)
     if [[ "$NET_STATUS" == "full" ]]; then
@@ -32,20 +28,19 @@ get_network(){
         printf "Network: Not Connected"
     fi
 }
-#
+
 get_ip(){
     echo ""
     printf "Your IP: "
     printf "$(ip address)" | grep "scope global" | awk '{print $2}'
 }
-#
+
 get_uptime(){
     printf "Uptime: " && uptime -p | awk '{print $2, $3, $4, $5}'
 }
 
 get_ram
 get_disk
-#get_cpu
 get_battery
 get_network
 get_ip
