509f84038cca8b6d9890a7e72687133342d1f59e
RangS-1 <rangga19sj@gmail.com>
2026-07-23 05:39:52 +0700

    Update: projectgenerator.sh. PROJECT_NAME & case logic

--- projectgenerator.sh
+++ projectgenerator.sh
@@ -8,3 +8,20 @@ fi
 echo "Template: $1"
 echo "Project : $2"
 
+PROJECT_NAME="$2"
+mkdir "$PROJECT_NAME"
+
+case "$1" in
+    "web")
+        echo "Web"
+        exit 1
+        ;;
+    "python")
+        echo "Python"
+        exit 1
+        ;;
+    "php")
+        echo "PHP"
+        exit 1
+        ;;
+esac
