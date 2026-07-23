2eb808a2b841cd73550cedb6278e095f49fa1b52
RangS-1 <rangga19sj@gmail.com>
2026-07-23 15:34:26 +0700

    Update: projectgenerator.sh. Create Python Template function

--- projectgenerator.sh
+++ projectgenerator.sh
@@ -8,12 +8,13 @@ fi
 echo "Template: $1"
 echo "Project : $2"
 
+TEMPLATE="$1"
+
 PROJECT_NAME="$2"
 mkdir "$PROJECT_NAME"
 
 create_web(){
     cd $PROJECT_NAME
-    touch index.html
     
     cat << EOF > index.html
 <!DOCTYPE HTML>
@@ -44,13 +45,34 @@ let element = document.getElementById('id');
 EOF
 }
 
-case "$1" in
+create_python(){
+    cd $PROJECT_NAME
+    cat << EOF > README.md
+# Readme
+This is Python Project
+EOF
+
+    mkdir src && cd src
+    mkdir appname && cd appname
+
+    cat << EOF > main.py
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
+EOF
+    cat << EOF > __init__.py
+EOF
+}
+
+case "$TEMPLATE" in
     "web")
         create_web
         exit 1
         ;;
     "python")
-        echo "Python"
+        create_python
         exit 1
         ;;
     "php")
