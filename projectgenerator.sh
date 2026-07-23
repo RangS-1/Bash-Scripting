c26307be32d8014ae22f80ff7c9afc268f783023
RangS-1 <rangga19sj@gmail.com>
2026-07-23 14:42:07 +0700

    Update: projectgenerator.sh. create_web function

--- projectgenerator.sh
+++ projectgenerator.sh
@@ -11,9 +11,42 @@ echo "Project : $2"
 PROJECT_NAME="$2"
 mkdir "$PROJECT_NAME"
 
+create_web(){
+    cd $PROJECT_NAME
+    touch index.html
+    
+    cat << EOF > index.html
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Website</title>
+</head>
+<body>
+    <h1>Title</h1>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
+EOF
+
+    mkdir css && cd css
+    cat << EOF > style.css
+:root{
+    color: white;
+}
+EOF
+
+    cd ..
+    mkdir js && cd js
+    cat << EOF > script.js
+let element = document.getElementById('id');
+EOF
+}
+
 case "$1" in
     "web")
-        echo "Web"
+        create_web
         exit 1
         ;;
     "python")
